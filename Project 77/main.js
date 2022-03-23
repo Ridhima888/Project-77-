06d35e16b92b23fb175cf7241cede95d38cda70f
Ridhima888 <85024192+Ridhima888@users.noreply.github.com>
2022-03-23 12:57:36 +0530

    Add files via upload

---
+++ Project 77/main.js
@@ -0,0 +1,24 @@
+var first_name= "Ridhima";
+var second_name= "Negi";
+
+var full_name=first_name.concat(second_name);
+console.log(full_name);
+
+var a= 8;
+var b= 6;
+
+var add= a+b;
+console.log("addition =" + add);
+
+
+
+var subtract= a-b;
+console.log("subtraction = " + subtract);
+
+
+
+var multiply = a*b;
+console.log("multiplication= " + multiply);
+
+var divide = a/b;
+console.log("division = " + divide);
